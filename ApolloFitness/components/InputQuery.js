ff9e6051d59df5973de71c1cf6b461515efc2ad7
KyleWalks <kyle.walks12@gmail.com>
2020-02-24 23:04:37 -0500

    Change file name of InputQuery.js

--- ApolloFitness/components/InputQuery.js
+++ ApolloFitness/components/InputQuery.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-class inputQuery extends Component {
+export default class InputQuery extends Component {
     render = () => {
         return (
             <View style={{
                 flexDirection: 'row', 
                 flex: 1, 
             }}>
-                <Text>{name}</Text>
-                <TextInput placeholder={placeholder}></TextInput>
+                <Text>{this.props.name}</Text>
+                <TextInput placeholder={this.props.placeholder}></TextInput>
             </View>
         );
     }
 }
-
-export default inputQuery;
--- ApolloFitness/screens/FoodScreen.js
+++ ApolloFitness/screens/FoodScreen.js
@@ -5,13 +5,20 @@ import {
   View,
 } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-import inputQuery from 'components/inputQuery.js';
+import inputQuery from '../components/InputQuery';
 
 export default function FoodScreen() {
   return (
-    <View>
-      <inputQuery name="Name", placeholder="Enter name here">
-      </inputQuery>
+    <View style={{
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <InputQuery name='Name' placeholder="Enter name here"/>
+      <InputQuery name='Name' placeholder="Enter name here"/>
+      <InputQuery name='Name' placeholder="Enter name here"/>
+      <InputQuery name='Name' placeholder="Enter name here"/>
+      <InputQuery name='Name' placeholder="Enter name here"/>
     </View>
   );
 }
