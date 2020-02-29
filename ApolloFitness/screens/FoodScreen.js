a13ebcece4bca603fc234f77c2836baac0b33547
KyleWalks <kyle.walks12@gmail.com>
2020-02-29 00:00:15 -0500

    Fix version issue

--- ApolloFitness/screens/FoodScreen.js
+++ ApolloFitness/screens/FoodScreen.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Button,
 } from 'react-native';
 import InputQuery from '../components/InputQuery';
 
@@ -19,15 +18,11 @@ export default class FoodScreen extends Component {
         <InputQuery name='Fat' placeholder="Enter fat here"/>
         <InputQuery name='Carbohydrates' placeholder="Enter carbohydrates here"/>
         <InputQuery name='Protein' placeholder="Enter protein here"/>
-
-        <Button
-          title="Add Item"
-        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  // Empty for now
+   // intentionally empty for now
 });
