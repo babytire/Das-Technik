2a227b119fbc34ae4c6d934a8b9b22058b3728c0
scottaudet <scott.audet@icloud.com>
2020-02-29 11:30:00 -0500

    created tab for profile screen and profileScreen component

---
+++ ApolloFitness/screens/ProfileScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+
+export default function ProfileScreen() {
+    return (
+        <View style = {styles.container}>
+            <Text>
+                Profile contents go here
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    //blank for now
+});
