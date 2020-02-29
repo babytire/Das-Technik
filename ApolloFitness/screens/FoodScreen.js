48d6e919e03210398009868e95519d5b6ac7789a
KyleWalks <kyle.walks12@gmail.com>
2020-02-29 00:00:15 -0500

    Fix version issue

--- ApolloFitness/screens/FoodScreen.js
+++ ApolloFitness/screens/FoodScreen.js
@@ -2,15 +2,8 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text,
 } from 'react-native';
-import { Button, ButtonGroup, Avatar } from 'react-native-elements';
 import InputQuery from '../components/InputQuery';
-import Table, { Section, StaticCell } from 'react-native-js-tableview';
-import { StatusBar } from 'react-native';
-import { Dimensions } from 'react-native';
-
-const moodOptions = 5
 
 export default class FoodScreen extends Component {
 
@@ -86,75 +79,21 @@ export default class FoodScreen extends Component {
     const { selectedIndex } = this.state
 
     return (
-      <>
-        <StatusBar
-					backgroundColor="black"
-					barStyle={'dark-content'} />
-
-        <View style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#1F1F1F',
-        }}>
-          <InputQuery style={{color:'white'}} name='Name' placeholder="Enter name here"/>
-          <InputQuery style={{color:'white'}} name='Calories' placeholder="Enter calories here" onChangeText={(text) => this.setState({entryCal: parseInt(text)})}/>
-          <InputQuery style={{color:'white'}} name='Fat' placeholder="Enter fat here" onChangeText={(text) => this.setState({entryFat: parseInt(text)})}/>
-          <InputQuery style={{color:'white'}} name='Carbohydrates' placeholder="Enter carbohydrates here" onChangeText={(text) => this.setState({entryCarbs: parseInt(text)})}/>
-          <InputQuery style={{color:'white'}} name='Protein' placeholder="Enter protein here" onChangeText={(text) => this.setState({entryProt: parseInt(text)})}/>
-        </View>
-        <View style={{
-          flex: 0,
-          backgroundColor: '#1F1F1F',
-        }}>
-          <Button title="Add Food" onPress={this._onAddFood} />
-          <ButtonGroup
-            containerStyle={{backgroundColor: '#333333'}}
-            onPress={this.updateIndex}
-            selectedIndex={selectedIndex}
-            buttons={buttons}
-          />
-        </View>
-
-        <View style={{
-          flex: 4,
-        }}>
-          <Table style={styles.container} theme="dark" accentColor="gold" scrollable={true}>
-            <Section header="Food Intake Rundown">
-              <StaticCell title={this.state.totalCal + " kcal"} subtitle="Calories" accessory="details" />
-              <StaticCell title={this.state.totalFat + "g"} subtitle="Fat" accessory="details" />
-              <StaticCell title={this.state.totalCarbs + "g"} subtitle="Carbohydrates" accessory="details" />
-              <StaticCell title={this.state.totalProt + "g"} subtitle="Protein" accessory="details" />
-            </Section>
-
-            <View style={{
-              flex: 1,
-              alignItems: 'center',
-            }}>
-              <Avatar
-                size="large"
-                rounded
-                title={buttons[this.state.currentMood]}
-                overlayContainerStyle={{backgroundColor: 'dark'}}
-              />
-              <Text style={{fontSize: 20, color: 'white'}}>Average Mood</Text>
-            </View>
-          </Table>
-        </View>
-      </>
+      <View style={{
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <InputQuery name='Name' placeholder="Enter name here"/>
+        <InputQuery name='Calories' placeholder="Enter calories here"/>
+        <InputQuery name='Fat' placeholder="Enter fat here"/>
+        <InputQuery name='Carbohydrates' placeholder="Enter carbohydrates here"/>
+        <InputQuery name='Protein' placeholder="Enter protein here"/>
+      </View>
     );
   };
 }
 
 const styles = StyleSheet.create({
-  container: {
-		flex: 1,
-		marginBottom: 30,
-  },
-  lightFont: {
-    color: 'white'
-  }
-
-  // Dark mode: #1F1F1F
+   // intentionally empty for now
 });
