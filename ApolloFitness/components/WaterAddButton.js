117479bd8d96a9c67f573296c67d8bad8f291f4d
Isaac Sparks-Willey <isaac.sparkswilley@maine.edu>
2020-03-10 17:12:31 -0400

    add slider styling

--- ApolloFitness/components/WaterAddButton.js
+++ ApolloFitness/components/WaterAddButton.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import * as Progress from "react-native-progress";
-import React, { Component } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-=======
 import * as Progress from 'react-native-progress';
 import React, { Component } from 'react';
 import {
@@ -13,50 +8,17 @@ import {
     StyleSheet,
     Slider,
  } from 'react-native';
->>>>>>> add basic slider functionality
 
 export default class WaterAddButton extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-<<<<<<< HEAD
-      ozDrank: 0
-=======
        ozDrank: 0,
        sliderState: 8,
->>>>>>> add basic slider functionality
     };
   }
 
-<<<<<<< HEAD
-  render = () => {
-    return (
-      <View style={{ flex: 1, justifyContent: "flex-end" }}>
-        <View style={styles.items}>
-          {/* Progress bar is out of 64 oz (8 glasses)
-                     until goals are added in a later issue */}
-          <Progress.Bar
-            style={styles.progressBar}
-            progress={this.state.ozDrank / 64}
-            width={250}
-            height={100}
-          />
-          <Text style={styles.text}>{this.state.ozDrank} oz</Text>
-        </View>
-
-        <View style={styles.button}>
-          <Button
-            title="Add Glass"
-            onPress={() =>
-              this.setState(prevState => ({ ozDrank: prevState.ozDrank + 8 }))
-            }
-          />
-        </View>
-      </View>
-    );
-  };
-=======
     render = () => {
         return (
           <View style={{flex: 1, justifyContent: 'flex-end'}}>
@@ -84,7 +46,7 @@ export default class WaterAddButton extends Component {
                     this.setState({sliderState: sliderValue})}
                 />
                 <Button
-                    title={"Add " + this.state.sliderState + " oz"}
+                    title="Add Glass"
                     onPress={() =>
                       this.setState({ozDrank: this.state.ozDrank + this.state.sliderState})}
                   />
@@ -92,36 +54,16 @@ export default class WaterAddButton extends Component {
             </View>
         );
     }
->>>>>>> fix progress bar and text layout
 }
 
 const styles = StyleSheet.create({
   button: {
-<<<<<<< HEAD
-    flex: 0.3,
-    justifyContent: "flex-end",
-=======
     flex: 1,
     justifyContent: 'flex-end',
->>>>>>> fix progress bar and text layout
     marginBottom: 36
   },
 
   items: {
-<<<<<<< HEAD
-    flexDirection: "row",
-    alignItems: "baseline"
-  },
-
-  progressBar: {
-    transform: [{ rotate: "270deg" }]
-  },
-
-  text: {
-    color: "blue",
-    fontWeight: "bold",
-    fontSize: 48
-=======
     flex: 10,
     flexDirection: 'column',
     justifyContent: 'flex-end',
@@ -138,6 +80,5 @@ const styles = StyleSheet.create({
     fontSize: 48,
     marginTop: 85,
     marginLeft: 40
->>>>>>> fix progress bar and text layout
   }
 });
