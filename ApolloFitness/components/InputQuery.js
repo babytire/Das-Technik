94b09bee5e1f6be31a96dcee0d8203680aa36e9f
KyleWalks <kyle.walks12@gmail.com>
2020-03-11 15:35:30 -0400

    add functional input and display of input

--- ApolloFitness/components/InputQuery.js
+++ ApolloFitness/components/InputQuery.js
@@ -2,17 +2,15 @@ import React, { Component } from "react";
 import { View, Text, TextInput } from "react-native";
 
 export default class InputQuery extends Component {
-  render = () => {
-    return (
-      <View
-        style={{
-          flexDirection: "row",
-          flex: 1
-        }}
-      >
-        <Text>{`${this.props.name}: `}</Text>
-        <TextInput placeholder={this.props.placeholder} />
-      </View>
-    );
-  };
+    render = () => {
+        return (
+            <View style={{
+                flexDirection: 'row', 
+                flex: 1, 
+            }}>
+                <Text>{this.props.name + ': '}</Text>
+                <TextInput placeholder={this.props.placeholder} onChangeText={this.props.onChangeText}></TextInput>
+            </View>
+        );
+    }
 }
