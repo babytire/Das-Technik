aa7289b65c7275e4ab33cdf3ab541ea75062018b
KyleWalks <kyle.walks12@gmail.com>
2020-03-19 21:50:45 -0400

    Add mood tracking

--- ApolloFitness/screens/FoodScreen.js
+++ ApolloFitness/screens/FoodScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from "react";
 import { StyleSheet, View, Text, StatusBar } from "react-native";
 import { Button, Avatar } from "react-native-elements";
@@ -5,6 +6,19 @@ import Table, { Section, StaticCell } from "react-native-js-tableview";
 
 import InputQuery from "../components/InputQuery";
 import MoodTracker from "../components/MoodTracker";
+=======
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import { Button, ButtonGroup } from 'react-native-elements';
+import InputQuery from '../components/InputQuery';
+import Table, { Section, StaticCell } from 'react-native-js-tableview';
+import { StatusBar } from 'react-native';
+
+const moodOptions = 5
+>>>>>>> Add mood tracking
 
 const moodOptions = 5;
 
@@ -23,6 +37,7 @@ export default class FoodScreen extends Component {
       entryCarbs: 0,
       entryProt: 0,
 
+<<<<<<< HEAD
       moods: [0, 0, 0, 0, 0],
       currentMood: 2,
     };
@@ -33,6 +48,59 @@ export default class FoodScreen extends Component {
 
   updateIndex(selectedIndex) {
     this.setState({ selectedIndex });
+=======
+      selectedIndex: 2,
+      
+      moods: [0, 0, 0, 0, 0],
+      currentMood: 2,
+    }
+
+    this.averageMood = this.averageMood.bind(this)
+    this.updateIndex = this.updateIndex.bind(this)
+  }
+
+  averageMood() {
+    var sum = 0
+    var avg = 2
+    var arr = this.state.moods
+    
+    arr.forEach(val => sum += val)
+    avg = sum / moodOptions
+    console.log(avg)
+
+    var diff = new Array(moodOptions)
+
+    for (var i = 0; i < moodOptions; i++) {
+      diff[i] = avg - arr[i]
+    }
+    var low = diff[0]
+    var index = 0
+    for (var j = 1; j < moodOptions; j++) {
+      if (diff[j] < low) {
+        low = diff[j]
+        index = j
+      }
+    }
+
+    this.setState({currentMood: index})
+  }
+
+  updateIndex (selectedIndex) {
+    this.setState({selectedIndex})
+  }
+
+  _onAddFood = () => {
+    this.setState({totalCal: this.state.totalCal + this.state.entryCal})
+    this.setState({totalFat: this.state.totalFat + this.state.entryFat})
+    this.setState({totalCarbs: this.state.totalCarbs + this.state.entryCarbs})
+    this.setState({totalProt: this.state.totalProt + this.state.entryProt})
+
+    var arr = this.state.moods
+    arr[this.state.selectedIndex] += 1
+    this.setState({moods: arr})
+
+    this.averageMood()
+>>>>>>> Add mood tracking
   }
 
   averageMood() {
@@ -76,115 +144,47 @@ export default class FoodScreen extends Component {
   };
 
   render = () => {
-    const buttons = ["😀", "🙂", "😐", "🙁", "😞"];
+    const buttons = ['😀', '🤣', '😎', '😂', '😁']
+    const { selectedIndex } = this.state
 
     return (
       <>
-        <StatusBar backgroundColor="black" barStyle="dark-content" />
+        <StatusBar
+					backgroundColor="black"
+					barStyle={'dark-content'} />
 
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#1F1F1F"
-          }}
-        >
-          <InputQuery
-            style={{ color: "white" }}
-            name="Name"
-            placeholder="Enter name here"
-          />
-          <InputQuery
-            style={{ color: "white" }}
-            name="Calories"
-            placeholder="Enter calories here"
-            onChangeText={text => this.setState({ entryCal: parseInt(text) })}
-          />
-          <InputQuery
-            style={{ color: "white" }}
-            name="Fat"
-            placeholder="Enter fat here"
-            onChangeText={text => this.setState({ entryFat: parseInt(text) })}
-          />
-          <InputQuery
-            style={{ color: "white" }}
-            name="Carbohydrates"
-            placeholder="Enter carbohydrates here"
-            onChangeText={text => this.setState({ entryCarbs: parseInt(text) })}
-          />
-          <InputQuery
-            style={{ color: "white" }}
-            name="Protein"
-            placeholder="Enter protein here"
-            onChangeText={text => this.setState({ entryProt: parseInt(text) })}
-          />
-        </View>
-        <View
-          style={{
-            flex: 0,
-            backgroundColor: "#1F1F1F"
-          }}
-        >
-          <Button title="Add Food" onPress={this.onAddFood} />
-            <MoodTracker 
-              style={styles.moodSection} 
-              containerStyle={{ backgroundColor: "#333333" }}
-              onPress={this.updateIndex}
-            />
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <InputQuery style={styles.textInput} name='Name' placeholder="Enter name here"/>
+          <InputQuery name='Calories' placeholder="Enter calories here" onChangeText={(text) => this.setState({entryCal: parseInt(text)})}/>
+          <InputQuery name='Fat' placeholder="Enter fat here" onChangeText={(text) => this.setState({entryFat: parseInt(text)})}/>
+          <InputQuery name='Carbohydrates' placeholder="Enter carbohydrates here" onChangeText={(text) => this.setState({entryCarbs: parseInt(text)})}/>
+          <InputQuery name='Protein' placeholder="Enter protein here" onChangeText={(text) => this.setState({entryProt: parseInt(text)})}/>
         </View>
 
-        <View
-          style={{
-            flex: 4
-          }}
-        >
-          <Table
-            style={styles.container}
-            theme="dark"
-            accentColor="gold"
-            scrollable
-          >
-            <Section header="Food Intake Rundown">
-              <StaticCell
-                title={`${this.state.totalCal} kcal`}
-                subtitle="Calories"
-                accessory="details"
-              />
-              <StaticCell
-                title={`${this.state.totalFat}g`}
-                subtitle="Fat"
-                accessory="details"
-              />
-              <StaticCell
-                title={`${this.state.totalCarbs}g`}
-                subtitle="Carbohydrates"
-                accessory="details"
-              />
-              <StaticCell
-                title={`${this.state.totalProt}g`}
-                subtitle="Protein"
-                accessory="details"
-              />
-            </Section>
+        <Button title="Add Food" onPress={this._onAddFood} />
+        <ButtonGroup
+          style={{flex: 1}}
+          onPress={this.updateIndex}
+          selectedIndex={selectedIndex}
+          buttons={buttons}
+        />
 
-            <View
-              style={{
-                flex: 1,
-                alignItems: "center"
-              }}
-            >
-              <Avatar
-                size="large"
-                rounded
-                title={buttons[this.state.currentMood]}
-                overlayContainerStyle={{ backgroundColor: "#1F1F1F" }}
-              />
-              <Text style={{ fontSize: 20, color: "white" }}>Average Mood</Text>
-            </View>
-          </Table>
-        </View>
+        <Table style={styles.container} theme="dark" accentColor="gold" scrollable={true}>
+          <Section header="Food Intake Rundown">
+            <StaticCell title={this.state.totalCal + " kcal"} subtitle="Calories" accessory="details" />
+            <StaticCell title={this.state.totalFat + "g"} subtitle="Fat" accessory="details" />
+            <StaticCell title={this.state.totalCarbs + "g"} subtitle="Carbohydrates" accessory="details" />
+            <StaticCell title={this.state.totalProt + "g"} subtitle="Protein" accessory="details" />
+          </Section>
+          <Section>
+            <StaticCell title={buttons[this.state.currentMood]}/>
+          </Section>
+        </Table>
       </>
     );
   };
@@ -192,16 +192,7 @@ export default class FoodScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginBottom: 30
-  },
-  moodSection: {
-    flex: 0,
-    backgroundColor: "#1F1F1F"
-  },
-  lightFont: {
-    color: "white"
-  }
-
-  // Dark mode: #1F1F1F
+		flex: 1,
+		marginBottom: 30,
+	},
 });
