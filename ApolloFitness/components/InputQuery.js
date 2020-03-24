b3b079edc92f1a1c3f564a82ad5b7e1099765099
KyleWalks <kyle.walks12@gmail.com>
2020-03-24 15:31:14 -0400

    ran lint

--- ApolloFitness/components/InputQuery.js
+++ ApolloFitness/components/InputQuery.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import { View, Text, TextInput } from "react-native";
 
 export default class InputQuery extends Component {
-    render = () => {
-        return (
-            <View style={{
-                flexDirection: 'row', 
-                flex: 1,
-            }}>
-                <Text style={this.props.style}>{this.props.name + ': '}</Text>
-                <TextInput placeholder={this.props.placeholder} onChangeText={this.props.onChangeText}></TextInput>
-            </View>
-        );
-    }
+  render = () => {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          flex: 1
+        }}
+      >
+        <Text style={this.props.style}>{`${this.props.name}: `}</Text>
+        <TextInput
+          placeholder={this.props.placeholder}
+          onChangeText={this.props.onChangeText}
+        />
+      </View>
+    );
+  };
 }
-
