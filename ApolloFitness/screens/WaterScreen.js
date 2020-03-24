9fdd0323cb6b33f16f078df7cc759c581d44a105
KyleWalks <kyle.walks12@gmail.com>
2020-03-24 15:31:14 -0400

    ran lint

--- ApolloFitness/screens/WaterScreen.js
+++ ApolloFitness/screens/WaterScreen.js
@@ -1,110 +1,91 @@
-
-import React from 'react';
-import {
-  StyleSheet,
-  AsyncStorage,
-  TouchableOpacity,
-} from 'react-native';
-import WaterAddButton from '../components/WaterAddButton'
+import React from "react";
+import { StyleSheet, AsyncStorage, TouchableOpacity } from "react-native";
+import WaterAddButton from "../components/WaterAddButton";
 
 export default class WaterScreen extends React.Component {
-    constructor(props) {
-	     super(props);
-    }
- 
-	saveWater = (date, water) => {
-		alert('entered')
-		this.setWaterStorage(date, water)
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	getWater = (date) => {
-		alert("entered")
-		this.getWaterStorage(date)
-		
-	}
+  saveWater = (date, water) => {
+    alert("entered");
+    this.setWaterStorage(date, water);
+  };
 
-	reomveWater = (date) => {
-		this.removeWaterStorage(date)
-	}
+  getWater = date => {
+    alert("entered");
+    this.getWaterStorage(date);
+  };
 
-	//this.setState({ozDrank: this.state.ozDrank + 8})}
+  reomveWater = date => {
+    this.removeWaterStorage(date);
+  };
 
+  // this.setState({ozDrank: this.state.ozDrank + 8})}
 
-    render() {
-	return (
-    	<View style={styles.bottom}>
-      	  <Text>
-		{this.state.ozDrank} oz
-      	  </Text>
-	  <Button
-            title="Press me"
-            onPress={() => this.setState({ozDrank: this.state.ozDrank + 8})}
-          />
+  render() {
+    return (
+      <View style={styles.bottom}>
+        <Text>{this.state.ozDrank} oz</Text>
+        <Button
+          title="Press me"
+          onPress={() => this.setState({ ozDrank: this.state.ozDrank + 8 })}
+        />
 
+        <Button
+          title="Save me"
+          onPress={() => this.saveWater("11/11/11", this.state.ozDrank)}
+        />
+        <Button title="Show me" onPress={() => this.getWater("11/11/11")} />
 
+        <TouchableOpacity onPress={this.saveWater("11/11/11", "15 Oz")}>
+          <Text> Click to Save</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.getWater("11/11/11")}>
+          <Text> Click to pull</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.removeWaterStorage("11/11/11")}>
+          <Text> Click to Remove</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
-          <Button
-	    title="Save me"
-	    onPress={() => this.saveWater('11/11/11', this.state.ozDrank)}
-          />
-	  <Button
-            title="Show me"
-            onPress={() => this.getWater('11/11/11')}
-          />
-
-	  <TouchableOpacity onPress = {this.saveWater('11/11/11', '15 Oz')}>
-		<Text> Click to Save</Text>
-	  </TouchableOpacity>
-	  <TouchableOpacity onPress = {this.getWater('11/11/11')}>
-                <Text> Click to pull</Text>
-          </TouchableOpacity>
-	  <TouchableOpacity onPress = {this.removeWaterStorage('11/11/11')}>
-                <Text> Click to Remove</Text>
-          </TouchableOpacity>
-	
-        </View>
-       );
-   }
-  
-  //Saves stored water data
+  // Saves stored water data
   setWaterStorage = async (date, water) => {
-	try {
-		await AsyncStorage.setItem(date, water);
-	}
-	catch (error) {
-		//error savind data
-		console.log("Saving Water Error")
-	}
+    try {
+      await AsyncStorage.setItem(date, water);
+    } catch (error) {
+      // error savind data
+      console.log("Saving Water Error");
+    }
   };
 
-  //retrieves saved water data	
-  getWaterStorage = async (date) => { 
-	try {
-		alert('entered2')
-		let gotWater = await AsyncStorage.getItem(date)
-		alert(gotWater)		
-		if (gotWater != null) {
-			alert(gotWater)
-			return gotWater
-		}
-		else {
-			console.log("No Water Data")
-		}
-	}
-	catch (error) {
-		//error reciving data
-		console.log("read data error")
-	}
+  // retrieves saved water data
+  getWaterStorage = async date => {
+    try {
+      alert("entered2");
+      const gotWater = await AsyncStorage.getItem(date);
+      alert(gotWater);
+      if (gotWater != null) {
+        alert(gotWater);
+        return gotWater;
+      }
+
+      console.log("No Water Data");
+    } catch (error) {
+      // error reciving data
+      console.log("read data error");
+    }
   };
 
-  //removes saved water data  
-  removeWaterStorage = async (date) => {
-        try {
-                AsyncStorage.removeItem(date)
-        }
-        catch (error) {
-                //error removing data
-                console.log("remove data error")
-        }
+  // removes saved water data
+  removeWaterStorage = async date => {
+    try {
+      AsyncStorage.removeItem(date);
+    } catch (error) {
+      // error removing data
+      console.log("remove data error");
+    }
   };
 }
