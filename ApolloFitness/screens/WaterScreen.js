42951a815bebf272eb75fcdaafe5717f895deee1
KyleWalks <kyle.walks12@gmail.com>
2020-04-13 19:57:49 -0400

    Fix overwrite issue

--- ApolloFitness/screens/WaterScreen.js
+++ ApolloFitness/screens/WaterScreen.js
@@ -1,91 +1,70 @@
-import React from "react";
-import { StyleSheet, AsyncStorage, TouchableOpacity } from "react-native";
-import WaterAddButton from "../components/WaterAddButton";
+import React from 'react';
+import {
+  StyleSheet,
+  AsyncStorage,
+} from 'react-native';
+import WaterAddButton from '../components/WaterAddButton'
 
 export default class WaterScreen extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+	     super(props);
+    }
+ 	//Call these functions and not the ones down below when you wish to save, retrieve, or remove water information. Date and Water must be strings!!
+	//Calls saveWaterStorage async function
+	saveWater = (date, water) => {
+		this.setWaterStorage(date, water)
+	}
+	//Calls getWaterStorage async function
+	getWater = (date) => {
+		this.getWaterStorage(date)
+	}
+	//Calls removeWaterStorage async function
+	removeWater = (date) => {
+		this.removeWaterStorage(date)
+	}
 
-  saveWater = (date, water) => {
-    alert("entered");
-    this.setWaterStorage(date, water);
-  };
-
-  getWater = date => {
-    alert("entered");
-    this.getWaterStorage(date);
-  };
-
-  reomveWater = date => {
-    this.removeWaterStorage(date);
-  };
-
-  // this.setState({ozDrank: this.state.ozDrank + 8})}
-
-  render() {
-    return (
-      <View style={styles.bottom}>
-        <Text>{this.state.ozDrank} oz</Text>
-        <Button
-          title="Press me"
-          onPress={() => this.setState({ ozDrank: this.state.ozDrank + 8 })}
-        />
-
-        <Button
-          title="Save me"
-          onPress={() => this.saveWater("11/11/11", this.state.ozDrank)}
-        />
-        <Button title="Show me" onPress={() => this.getWater("11/11/11")} />
-
-        <TouchableOpacity onPress={this.saveWater("11/11/11", "15 Oz")}>
-          <Text> Click to Save</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.getWater("11/11/11")}>
-          <Text> Click to pull</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.removeWaterStorage("11/11/11")}>
-          <Text> Click to Remove</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
-  // Saves stored water data
+    render() {
+	     return (
+         <WaterAddButton />
+       );
+   }
+  
+  //Saves stored water data
   setWaterStorage = async (date, water) => {
-    try {
-      await AsyncStorage.setItem(date, water);
-    } catch (error) {
-      // error savind data
-      console.log("Saving Water Error");
-    }
+	try {
+		await AsyncStorage.setItem(date, water);
+	}
+	catch (error) {
+		//error savind data
+		console.log("Saving Water Error")
+	}
   };
 
-  // retrieves saved water data
-  getWaterStorage = async date => {
-    try {
-      alert("entered2");
-      const gotWater = await AsyncStorage.getItem(date);
-      alert(gotWater);
-      if (gotWater != null) {
-        alert(gotWater);
-        return gotWater;
-      }
-
-      console.log("No Water Data");
-    } catch (error) {
-      // error reciving data
-      console.log("read data error");
-    }
+  //retrieves saved water data	
+  getWaterStorage = async (date) => { 
+	try {
+		let gotWater = await AsyncStorage.getItem(date)
+		if (gotWater != null) {
+			return gotWater
+		}
+		else {
+			console.log("No Water Data")
+		}
+	}
+	catch (error) {
+		//error reciving data
+		console.log("read data error")
+	}
   };
 
-  // removes saved water data
-  removeWaterStorage = async date => {
-    try {
-      AsyncStorage.removeItem(date);
-    } catch (error) {
-      // error removing data
-      console.log("remove data error");
-    }
+  //removes saved water data  
+  removeWaterStorage = async (date) => {
+        try {
+                AsyncStorage.removeItem(date)
+        }
+        catch (error) {
+                //error removing data
+                console.log("remove data error")
+        }
   };
 }
